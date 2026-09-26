2776763cbfc22cfd7f46c4dd05472f954f26081d
Ricardocaxito6-byte <ricardocaxito6@gmail.com>
2026-09-26 16:50:10 +0100

    Create Config.js

---
+++ js/Config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://svjwpenvlsxqfqeekkyy.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_kGUBjtGquR2hw6D5IvKUVw_pwLu-vk9";
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
